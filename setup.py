c78ec46e8b1ba30a469040cfe68d24e39f4f1a15
LeptoFlare <leptoflare@gmail.com>
2021-02-07 18:16:48 -0500

    Update setup.py - Mirror package to blurple and blurple.py

--- setup.py
+++ setup.py
@@ -13,22 +13,22 @@ with open('README.md') as file:
     readme = file.read()
 
 
-setup(name='blurple.py',
-      author='LeptoFlare',
-      url='https://github.com/LeptoFlare/blurple.py',
-      project_urls={
+kwargs = {
+    "author": 'LeptoFlare',
+    "url": 'https://github.com/LeptoFlare/blurple.py',
+    "project_urls": {
         "Issues": "https://github.com/LeptoFlare/blurple.py/issues",
         "Docs": "https://lepto.tech/blurple.py",
-      },
-      version=version,
-      packages=['blurple'],
-      license='MIT',
-      description='A framework built on discord.py',
-      long_description=readme,
-      long_description_content_type="text/markdown",
-      include_package_data=True,
-      install_requires=requirements,
-      classifiers=[
+    },
+    "version": version,
+    "packages": ['blurple'],
+    "license": 'MIT',
+    "description": 'A framework built on discord.py',
+    "long_description": readme,
+    "long_description_content_type": "text/markdown",
+    "include_package_data": True,
+    "install_requires": requirements,
+    "classifiers": [
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
@@ -37,5 +37,9 @@ setup(name='blurple.py',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
-      ]
-)
+    ]
+}
+
+
+setup(name='blurple.py', **kwargs)
+setup(name='blurple', **kwargs)
