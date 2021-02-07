88f8a6abae6919ac8d990c3cf6bf13d6d6a031f1
LeptoFlare <leptoflare@gmail.com>
2021-02-07 13:22:12 -0500

    Update tests

--- blurple/test.py
+++ blurple/test.py
@@ -15,14 +15,22 @@ async def on_message(message):
         return
 
     # Test alerts
-    if message.content.startswith('?alert'):
-        await message.channel.send(embed=ui.Alert("primary", "This is a test alert", "Check it out!", name=False))
-        await message.channel.send(embed=ui.Alert("secondary", "This is a test alert", "Check it out!", name=False))
+    if message.content == '.alert style':
+        await message.channel.send(embed=ui.Alert("primary", "This is a test alert", "Check it out!"))
+        await message.channel.send(embed=ui.Alert("secondary", "This is a test alert", "Check it out!"))
         await message.channel.send(embed=ui.Alert("success", "This is a test alert", "Check it out!"))
-        await message.channel.send(embed=ui.Alert("danger", "This is a test alert", "Check it out!", name="Error"))
-        await message.channel.send(embed=ui.Alert("warning", "This is a test alert", "Check it out!", emoji=False))
+        await message.channel.send(embed=ui.Alert("danger", "This is a test alert", "Check it out!"))
+        await message.channel.send(embed=ui.Alert("warning", "This is a test alert", "Check it out!"))
         await message.channel.send(embed=ui.Alert("info", "This is a test alert", "Check it out!"))
-        await message.channel.send(embed=ui.Alert("light", "This is a test alert", "Check it out!", name=False))
-        await message.channel.send(embed=ui.Alert("dark", "This is a test alert", "Check it out!", name=False))
+        await message.channel.send(embed=ui.Alert("light", "This is a test alert", "Check it out!"))
+        await message.channel.send(embed=ui.Alert("dark", "This is a test alert", "Check it out!"))
+    if message.content == '.alert custom':
+        await message.channel.send(embed=ui.Alert("primary", "Custom Alerts", "Default style"))
+        await message.channel.send(embed=ui.Alert("primary", "Custom Alerts", "Alternate name", name="Alternate"))
+        await message.channel.send(embed=ui.Alert("primary", "Custom Alerts", "No name", name=False))
+        await message.channel.send(embed=ui.Alert("primary", "Custom Alerts", "No emoji", emoji=False))
+        await message.channel.send(embed=ui.Alert("primary", "Custom Alerts", "No emoji, alternate name", emoji=False, name="Alternate"))
+        await message.channel.send(embed=ui.Alert("primary", "Custom Alerts", "No emoji, no name", emoji=False, name=False))
+
 
 client.run(os.getenv("TOKEN"))
