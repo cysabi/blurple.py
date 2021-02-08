298df9aa9ad5e980bf5e4b62effc6c29fc7edf69
LeptoFlare <leptoflare@gmail.com>
2021-02-08 18:49:23 -0500

    Update alert styles to be stored in an Enum

--- blurple/ui/alert.py
+++ blurple/ui/alert.py
@@ -1,34 +1,37 @@
+from enum import Enum
 import discord
 
 
 class Alert(discord.Embed):
 
-    styles = {
-        "primary": (0x7289DA, "<:primary:807729299603980308>"),
-        "secondary": (0x99AAB5, "<:secondary:807729299531759637>"),
-        "success": (0x77B255, "\u2705"),
-        "danger": (0xDD2E44, "\U0001f6ab"),
-        "warning": (0xFFCC4D, "\u26a0\ufe0f"),
-        "info": (0x3B88C3, "\u2139\ufe0f"),
-        "light": (0xE6E7E8, "\U0001f533"),
-        "dark": (0x31373D, "\U0001f532"),
-    }
+    class Style(Enum):
+        PRIMARY = (0x7289DA, "<:primary:807729299603980308>")
+        SECONDARY = (0x99AAB5, "<:secondary:807729299531759637>")
+        SUCCESS = (0x77B255, "\u2705")
+        DANGER = (0xDD2E44, "\U0001f6ab")
+        WARNING = (0xFFCC4D, "\u26a0\ufe0f")
+        INFO = (0x3B88C3, "\u2139\ufe0f")
+        LIGHT = (0xE6E7E8, "\U0001f533")
+        DARK = (0x31373D, "\U0001f532")
 
-    def __init__(self, style, title: str, description: str = discord.Embed.Empty, **kwargs):
+        def __getitem__(self, key):
+            return self.value[key]
+
+    def __init__(self, style: Style, title: str, description: str = discord.Embed.Empty, **kwargs):
         super().__init__(
-            color=self.styles[style][0],
+            color=style[0],
             title=self.process_title(style, title, **kwargs),
             description=description
         )
 
     @classmethod
-    def process_title(cls, style, title, **kwargs):
-        output = ''
+    def process_title(cls, style: Style, title: str, **kwargs):
+        output: str = ''
 
         if kwargs.get("emoji") is not False:
-            output += cls.styles[style][1] + " "
+            output += style[1] + " "
 
-        if (name := kwargs.get("name", style.capitalize())) is not False:
+        if (name := kwargs.get("name", style.name.capitalize())) is not False:
             output += f"`{name}:` "
 
         return output + f"**{title}**"
--- test.py
+++ test.py
@@ -13,7 +13,7 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send(embed=ui.Alert("success",
+    await ctx.send(embed=ui.Alert(ui.Alert.Style.SUCCESS,
         title="Pong!",
         description=f"Latency: `{round(bot.latency*1000)}ms`")
     )
@@ -21,21 +21,21 @@ async def ping(ctx):
 @bot.command()
 async def alert(ctx, sub="styles"):
     if sub == "styles":
-        await ctx.send(embed=ui.Alert("primary", "This is a test alert", "Check it out!"))
-        await ctx.send(embed=ui.Alert("secondary", "This is a test alert", "Check it out!"))
-        await ctx.send(embed=ui.Alert("success", "This is a test alert", "Check it out!"))
-        await ctx.send(embed=ui.Alert("danger", "This is a test alert", "Check it out!"))
-        await ctx.send(embed=ui.Alert("warning", "This is a test alert", "Check it out!"))
-        await ctx.send(embed=ui.Alert("info", "This is a test alert", "Check it out!"))
-        await ctx.send(embed=ui.Alert("light", "This is a test alert", "Check it out!"))
-        await ctx.send(embed=ui.Alert("dark", "This is a test alert", "Check it out!"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "This is a test alert", "Check it out!"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.SECONDARY, "This is a test alert", "Check it out!"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.SUCCESS, "This is a test alert", "Check it out!"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.DANGER, "This is a test alert", "Check it out!"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.WARNING, "This is a test alert", "Check it out!"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.INFO, "This is a test alert", "Check it out!"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.LIGHT, "This is a test alert", "Check it out!"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.DARK, "This is a test alert", "Check it out!"))
     if sub == "custom":
-        await ctx.send(embed=ui.Alert("primary", "Custom Alerts", "Default style"))
-        await ctx.send(embed=ui.Alert("primary", "Custom Alerts", "Alternate name", name="Alternate"))
-        await ctx.send(embed=ui.Alert("primary", "Custom Alerts", "No name", name=False))
-        await ctx.send(embed=ui.Alert("primary", "Custom Alerts", "No emoji", emoji=False))
-        await ctx.send(embed=ui.Alert("primary", "Custom Alerts", "No emoji, alternate name", emoji=False, name="Alternate"))
-        await ctx.send(embed=ui.Alert("primary", "Custom Alerts", "No emoji, no name", emoji=False, name=False))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "Default style"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "Alternate name", name="Alternate"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "No name", name=False))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "No emoji", emoji=False))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "No emoji, alternate name", emoji=False, name="Alternate"))
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "No emoji, no name", emoji=False, name=False))
 
 
 bot.run(os.getenv("TOKEN"))
