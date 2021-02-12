c83d27936e6a978e96d91028c8c1806def8fcba1
LeptoFlare <leptoflare@gmail.com>
2021-02-12 15:39:04 -0500

    Update test command

--- test.py
+++ test.py
@@ -1,39 +1,17 @@
 import os
 from discord.ext import commands
 
-from blurple import ui, io
-
-bot = commands.Bot(command_prefix='!')
+from blurple import ui, io, ext
 
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name}: Ready for Testing')
+class Testing(commands.Cog, command_attrs={"invoke_without_command": True}):
 
-@bot.command()
-async def reply(ctx, sub="message"):
-    if sub == "message":
-        await ctx.send("Enter a number.")
-        reply = await io.MessageReply(ctx, validate=r'^[0-9]{1,}$').result()
-        await ctx.send(embed=ui.Alert(ui.Alert.Style.SUCCESS, "Valid Reply", reply.content))
-    if sub == "reaction":
-        message = await ctx.send("Enter reaction.")
-        reply = await io.ReactionAddReply(ctx,
-            validate=["<:primary:808874731763007488>", "<:secondary:808874731758813205>"],
-            message=message).result()
-        await ctx.send(embed=ui.Alert(ui.Alert.Style.SUCCESS, "Valid Reply", str(reply.emoji)))
+    @commands.group()
+    async def alert(self, ctx):
+        pass
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send(embed=ui.Alert(ui.Alert.Style.SUCCESS,
-        title="Pong!",
-        description=f"Latency: `{round(bot.latency*1000)}ms`")
-    )
-
-@bot.command()
-async def alert(ctx, sub="styles"):
-    if sub == "styles":
+    @alert.command()
+    async def styles(self, ctx):
         await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "This is a test alert", "Check it out!"))
         await ctx.send(embed=ui.Alert(ui.Alert.Style.SECONDARY, "This is a test alert", "Check it out!"))
         await ctx.send(embed=ui.Alert(ui.Alert.Style.SUCCESS, "This is a test alert", "Check it out!"))
@@ -42,7 +20,9 @@ async def alert(ctx, sub="styles"):
         await ctx.send(embed=ui.Alert(ui.Alert.Style.INFO, "This is a test alert", "Check it out!"))
         await ctx.send(embed=ui.Alert(ui.Alert.Style.LIGHT, "This is a test alert", "Check it out!"))
         await ctx.send(embed=ui.Alert(ui.Alert.Style.DARK, "This is a test alert", "Check it out!"))
-    if sub == "custom":
+
+    @alert.command()
+    async def custom(self, ctx):
         await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "Default style"))
         await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "Alternate name", name="Alternate"))
         await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "No name", name=False))
@@ -50,5 +30,37 @@ async def alert(ctx, sub="styles"):
         await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "No emoji, alternate name", emoji=False, name="Alternate"))
         await ctx.send(embed=ui.Alert(ui.Alert.Style.PRIMARY, "Custom Alerts", "No emoji, no name", emoji=False, name=False))
 
+    @commands.group()
+    async def reply(self, ctx):
+        pass
+
+    @reply.command()
+    async def message(self, ctx):
+        await ctx.send("Enter a number.")
+        reply = await io.MessageReply(ctx, validate=r'^[0-9]{1,}$').result()
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.SUCCESS, "Valid Reply", reply.content))
+
+    @reply.command(aliases=["react"])
+    async def reaction(self, ctx):
+        message = await ctx.send("Enter reaction.")
+        reply = await io.ReactionAddReply(ctx,
+            validate=["<:primary:808874731763007488>", "<:secondary:808874731758813205>"],
+            message=message).result()
+        await ctx.send(embed=ui.Alert(ui.Alert.Style.SUCCESS, "Valid Reply", str(reply.emoji)))
+
+
+bot = commands.Bot(command_prefix='!')
+bot.help_command = ext.HelpCommand(ui.Alert.Style.PRIMARY[0])
+bot.add_cog(Testing())
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name}: Ready for Testing')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(embed=ui.Alert(ui.Alert.Style.SUCCESS,
+        title="Pong!",
+        description=f"Latency: `{round(bot.latency*1000)}ms`"))
 
 bot.run(os.getenv("TOKEN"))
