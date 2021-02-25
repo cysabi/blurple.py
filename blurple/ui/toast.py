9da019d37a681432bc0b00383fc6e8d0de6585e9
LeptoFlare <leptoflare@lepto.tech>
2021-02-25 14:15:51 -0500

    Create ui.Toast

---
+++ blurple/ui/toast.py
@@ -0,0 +1,31 @@
+from .style import Style
+import discord
+
+
+class Toast(discord.Embed):
+    """ A subclass of :class:`discord.Embed` for stylish toasts.
+
+        The difference between this class and :class:`Alert` is these are intended to hold more deemphasized information.
+        These are used with :class:`blurple.io.Toast`, which supports a "dismiss" button to dismiss the toast, and automatic deletion with specified duration.
+
+        :param Style style: The style of the toasts.
+        :param str text: The text of the toasts, will be wrapped in an emoji unless specified in options.
+        :param **options: Toast options to customize it's look.
+
+            :emoji: Defaults to :class:`True`. Can be set to false to remove the emoji from the toast.
+    """
+
+    def __init__(self, style: Style, text: str, **options):
+        super().__init__(
+            color=style[0],
+            description=self.process_text(style, text, **options),
+        )
+
+    @classmethod
+    def process_text(cls, style: Style, text: str, **options):
+        output: str = ''
+
+        if options.get("emoji") is not False:
+            output += style[1] + " "
+
+        return output + text
