786b3905797778fec372917af1533506d1be721c
LeptoFlare <leptoflare@lepto.tech>
2021-02-25 14:16:37 -0500

    Add io.Toast

--- blurple/io/__init__.py
+++ blurple/io/__init__.py
@@ -1,5 +1,2 @@
-"""Robust functions that enable you to build stable, multi-step commands with ease."""
-
 from .reply import Reply, MessageReply, ReactionAddReply
-
-__all__ = ["Reply", "MessageReply", "ReactionAddReply"]
+from .toast import Toast
---
+++ blurple/io/toast.py
@@ -0,0 +1,2 @@
+class Toast():
+    pass
