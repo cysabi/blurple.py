92774cf2f2930d02930e4c427d8b5fc68f61a47f
LeptoFlare <leptoflare@lepto.tech>
2021-02-25 17:58:05 -0500

    Fix issues when overwriting commands in `router` using `.callback` Now recreates entire command group object

--- blurple/ext/router.py
+++ blurple/ext/router.py
@@ -32,18 +32,25 @@ class Router:
         # Try and find group
         group = self.bot.get_command(" ".join(path))
         # If it doesn't exist, create it group
-        if group is None:
+        if group is None or func:
             # Get the parent group
             parent = self.get_command_group(path[:-1])
+            existing_subcommands = []
             # Create the func if it doesn't exist
             if func is None:
                 async def func(ctx):
                     raise commands.CommandNotFound(f'Command "{path[-1]}" is not found')
+            # Remove the current group it already exists
+            elif group is not None:
+                existing_subcommands = group.commands
+                parent.remove_command(group.name)
+
             # Create the group
             group = commands.Group(func, name=path[-1], invoke_without_command=True, **kwargs)
+            # Add existing subcommands
+            for cmd in existing_subcommands:
+                group.add_command(cmd)
+            # Register command
             parent.add_command(group)
-        # If it does exist, but needs to be overwritten, update callback
-        elif func:
-            group.callback = func
 
         return group
